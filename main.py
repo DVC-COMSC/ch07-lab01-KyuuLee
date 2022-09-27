9bcd0ef841981523886cea884ffd30df72c8b437
Kyuu <69904775+KyuuLee@users.noreply.github.com>
2022-09-26 23:49:29 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 
-# ******************************
-# Make your Code
-# ******************************
+sum = 0
+numbers = [0] * 5
+for i in range(5):
+	numbers[i] = int(input('Enter the number: '))
+	if i == 0:
+		min = max = numbers[i] 
+	else:
+		if (min > numbers[i]):
+			min = numbers[i] 
+		elif (max < numbers[i]):
+			max =  numbers[i]
+	sum += numbers[i]
+sum = sum - min - max
+# print ('Total : ', sum, 'Average: ', sum / 5.0)
+print (sum)
