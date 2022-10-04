a109f3e8e4cc9f9392657a0368f7ca185c8bdb39
Kayw Lee <kwooler@gmail.com>
2022-10-04 13:23:30 -0700

    1

--- main.py
+++ main.py
@@ -2,15 +2,17 @@
 sum = 0
 numbers = [0] * 5
 for i in range(5):
-	numbers[i] = int(input('Enter the number: '))
-	if i == 0:
-		min = max = numbers[i] 
-	else:
-		if (min > numbers[i]):
-			min = numbers[i] 
-		elif (max < numbers[i]):
-			max =  numbers[i]
-	sum += numbers[i]
+    numbers[i] = int(input('Enter the number: '))
+    if i == 0:
+        min = max = numbers[i]
+    else:
+        if (min > numbers[i]):
+            min = numbers[i]
+        elif (max < numbers[i]):
+            max = numbers[i]
+    sum += numbers[i]
 sum = sum - min - max
 # print ('Total : ', sum, 'Average: ', sum / 5.0)
-print (sum)
+print(sum)
+#
+#
